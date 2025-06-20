934c64d7c79e978b0e742d4579cda661da0b4699
Zhi <zhi.lu@usach.cl>
2025-06-20 11:56:21 -0400

    Se insertan valores a algunas tablas

--- SQL_avance.sql
+++ SQL_avance.sql
@@ -1,5 +1,4 @@
-
-  CREATE TABLE IF NOT EXISTS valoracion (
+CREATE TABLE IF NOT EXISTS valoracion (
   id_valoracion           			BIGINT    PRIMARY KEY,
   id_producto      			BIGINT,
   comentario_valoracion		VARCHAR(300),
@@ -7,10 +6,7 @@
   puntuacion				NUMERIC(2,1)
   
   --FALTA LLAVE FORANEA CON PRODUCTO
-  
 );
-
-
 
 CREATE TABLE IF NOT EXISTS rol (
   id_rol           			BIGINT    PRIMARY KEY,
@@ -23,12 +19,10 @@ CREATE TABLE IF NOT EXISTS tienda(
 	id_rol		BIGINT,
 	nombre_tienda	VARCHAR(32),
 	direccion_tienda	VARCHAR(255),
-	url_producto_imagen	CHAR(64)
+	url_producto_imagen	CHAR(64),
 
-	--FALTA  FOREIGN KEY
+	FOREIGN KEY (id_rol) REFERENCES rol(id_rol)
 );
-
-
 
 CREATE TABLE IF NOT EXISTS usuario (
   id_usuario            BIGINT       PRIMARY KEY,
@@ -50,15 +44,13 @@ CREATE TABLE IF NOT EXISTS carrito(
   productos_carro      INTEGER
 );
 
-
 CREATE TABLE IF NOT EXISTS carrito_usuario (
   id_usuario   BIGINT NOT NULL,
   id_carrito   BIGINT NOT NULL,
  
   PRIMARY KEY (id_usuario, id_carrito),
   FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
-  FOREIGN KEY (id_carrito) REFERENCES carrito(id_carrito)
-    
+  FOREIGN KEY (id_carrito) REFERENCES carrito(id_carrito)  
 );
 
 CREATE TABLE IF NOT EXISTS medio_de_pago (
@@ -68,7 +60,6 @@ CREATE TABLE IF NOT EXISTS medio_de_pago (
   url_medio            CHAR(64),
 
   FOREIGN KEY (id_carrito) REFERENCES carrito(id_carrito)
-    
 );
 
 CREATE TABLE IF NOT EXISTS boleta (
@@ -76,7 +67,6 @@ CREATE TABLE IF NOT EXISTS boleta (
   direccion_boleta        VARCHAR(255),
   valor_boleta            INTEGER,
   numero_boleta           BIGINT
-
 );
 
 CREATE TABLE IF NOT EXISTS compra (
@@ -92,10 +82,7 @@ CREATE TABLE IF NOT EXISTS compra (
   FOREIGN KEY (id_carrito) REFERENCES carrito(id_carrito),
   FOREIGN KEY (id_boleta) REFERENCES boleta(id_boleta),
   FOREIGN KEY (id_medio) REFERENCES medio_de_pago
- 
 );
-
-
 
 CREATE TABLE IF NOT EXISTS tienda_producto (
   id_tienda BIGINT NOT NULL,
@@ -105,7 +92,6 @@ CREATE TABLE IF NOT EXISTS tienda_producto (
   PRIMARY KEY (id_tienda, id_producto),
   FOREIGN KEY (id_tienda) REFERENCES tienda(id_tienda),
   FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
-
 );
 
 CREATE TABLE IF NOT EXISTS boleta_producto (
@@ -116,14 +102,12 @@ CREATE TABLE IF NOT EXISTS boleta_producto (
   PRIMARY KEY (id_boleta, id_producto),
   FOREIGN KEY (id_boleta) REFERENCES boleta(id_boleta),
   FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
-	
 );
 
 CREATE TABLE IF NOT EXISTS listadeseos (
   id_lista_deseos BIGINT PRIMARY KEY,
   productos_en_descuento INTEGER,
   cantidad_productos INTEGER
-
 );
 
 CREATE TABLE IF NOT EXISTS listadeseos_usuario (
@@ -134,7 +118,6 @@ CREATE TABLE IF NOT EXISTS listadeseos_usuario (
   PRIMARY KEY (id_usuario, id_lista_deseos),
   FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
   FOREIGN KEY (id_lista_deseos) REFERENCES listadeseos(id_lista_deseos)
-	
 );
 
 CREATE TABLE IF NOT EXISTS carrito_producto (
@@ -144,8 +127,7 @@ CREATE TABLE IF NOT EXISTS carrito_producto (
   --REVISAR
   PRIMARY KEY (id_carrito, id_producto),
   FOREIGN KEY (id_carrito) REFERENCES carrito(id_carrito),
-  FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
-	
+  FOREIGN KEY (id_producto) REFERENCES producto(id_producto)	
 );
 
 INSERT INTO rol (id_rol, tipo_rol, nivel_permiso) VALUES
@@ -153,10 +135,7 @@ INSERT INTO rol (id_rol, tipo_rol, nivel_permiso) VALUES
   (2, 'Jefe',  1),
   (3, 'Cliente',   2);
 
-
-
-INSERT INTO usuario (id_usuario,id_rol,nombre_usuario,apellido,email,contraseña,direccion_usuario,fecha_de_registro
-) VALUES
+INSERT INTO usuario (id_usuario,id_rol,nombre_usuario,apellido,email,contraseña,direccion_usuario,fecha_de_registro) VALUES
   ( 1,  3, 'Juan',      'Pérez',      'juan.perez@gmail.com',       'gjanhbjkju',           'Calle 1 Poniente 123, Santiago',         '2025-06-20'),
   ( 2,  3, 'Ana',       'González',   'ana.gonzalez@gmail.com',     'rnjkfjbhgy',           'Av. Libertador B. O´Higgins 1000, Santiago','2025-06-19'),
   ( 3,  3, 'Pedro',     'Muñoz',      'pedro.munoz@correo.cl',      'hbhjadhdhjba',         'Pasaje Los Robles 45, Valparaíso',       '2025-06-18'),
@@ -167,3 +146,27 @@ INSERT INTO usuario (id_usuario,id_rol,nombre_usuario,apellido,email,contraseña
   ( 8,  2, 'Valentina', 'Herrera',    'valentina.herrera@correo.cl','contraseña',           'Av. Kennedy 5000, Vitacura',               '2025-06-13'),
   ( 9,  3, 'Sebastián', 'Díaz',       'sebastian.diaz@correo.cl',   'albocampeon',          'Calle José Miguel Carrera 333, Talca',     '2025-06-12'),
   ( 10, 2, 'Camila',    'Fuentes',    'camila.fuentes@correo.cl',   'necesitoplata',        'Av. Manuel Montt 1500, Providencia',      '2025-06-11');
+
+INSERT INTO tienda (id_tienda, id_rol, nombre_tienda, direccion_tienda, url_producto_imagen) VALUES
+  (1, 2, 'PachoJuegos', 'Avenida Calamari', 'URL1'),
+  (2, 2, 'JuegosAdiccion', 'Monte Everest', 'URL2'),
+  (3, 2, 'TiendaMercy', 'Pasillo Tarantino', 'URL3'),
+  (4, 2, 'FisicoBGames', 'Avenida Estrellas', 'URL4'),
+  (5, 2, 'GamesFamily', 'Calle Balatro', 'URL5'),
+  (6, 2, 'RealJuegos', 'Avenida Calamari', 'URL6'),
+  (7, 2, 'SaintGames', 'Paso de los Santos', 'URL7'),
+  (8, 2, 'BalatroCastle', 'Camino del Joker', 'URL8'),
+  (9, 2, 'DarkGames', 'Avenida Estrellas', 'URL9'),
+  (10, 2, 'Ultimo Juego', 'Calle Balatro', 'URL10');
+
+ INSERT INTO medio_de_pago (id_medio, id_carrito, nombre_medio, url_medio) VALUES
+  (1, 3, 'Debito', 'URL1'),
+  (2, 1, 'Credito', 'URL2'),
+  (1, 4, 'Debito', 'URL1'),
+  (1, 6, 'Debito', 'URL1'),
+  (3, 8, 'Transferencia', 'URL3'),
+  (1, 2, 'Debito', 'URL1'),
+  (1, 9, 'Debito', 'URL1'),
+  (2, 10, 'Credito', 'URL2'),
+  (1, 7, 'Debito', 'URL1'),
+  (2, 5, 'Credito', 'URL2');
