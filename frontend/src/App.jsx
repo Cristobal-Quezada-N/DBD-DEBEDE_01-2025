ab122f8479545b7223c3881b6a5e3d1124b0e389
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-06-30 21:01:33 -0400

    feat(frontend): Agregar estructura de frontend
    Agregar estructura incial para el desarrollo frontend de la plataforma.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, React } from 'react'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+                <h1>Home</h1>
+        </>
+    )
+}
+
+export default App
