943eebc3f1d2b03bb43d9eca11abddd1e18cd838
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-07-01 20:27:52 -0400

    feat(buscador): Agregar seccion de busqueda
    Agregar seccion para busqueda en `NavBar`.
    Aplicar diseño adaptativo.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,26 +1,63 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown, Offcanvas } from 'react-bootstrap'
 
 function NavBar() {
     return (
-        <Navbar bg='dark' variant='dark' expand='lg'>
+        <Navbar variant='dark' expand='lg' className='bg-dark'>
             <Navbar.Brand as={Link} to='/'>TCG & Board Games</Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-                <Nav className='me-auto'>
-                    <NavDropdown title='Categorías' id='basic-nav-dropdown'>
-                        <NavDropdown.Item as={Link} to='/cartas'>TCG</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to='/mesa'>Board Games</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                <Nav>
+            <Nav className='d-none d-lg-flex me-auto'>
+                <NavDropdown title='Categorías' id='basic-nav-dropdown'>
+                    <NavDropdown.Item as={Link} to='/cartas'>TCG</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to='/mesa'>Board Games</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+
+            <Nav className='d-none d-md-flex mx-auto text-white'>
+                <p className='my-auto'>Buscador</p>
+            </Nav>
+
+            <Nav className='ms-auto align-items-center flex-row'>
+                <Nav.Link as={Link} to='/carrito' className='px-3'>
+                    <i className='bi-cart-fill'></i>
+                </Nav.Link>
+                <Navbar.Toggle aria-controls='offcanvasNavbar'/>
+                <Navbar.Offcanvas
+                    id='offcanvasNavbar'
+                    aria-labelledby='offcanvasNavbarLabel'
+                    placement='end'
+                    className='bg-dark text-white'
+                >
+                    <Offcanvas.Header closeButton closeVariant='white'>
+                        <Offcanvas.Title id='offcanvasNavbarLabel'>Tienda</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <div className='d-lg-none'>
+                            <p>Buscador</p>
+                        </div>
+
+                        <Nav className='d-lg-none'>
+                            <NavDropdown title='Categorías' id='dropdown-categorias'>
+                                <NavDropdown.Item as={Link} to='/cartas'>TCG</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to='/mesa'>Board Games</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+
+                        <div className='d-lg-none'>
+                            <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+                            <Nav.Link as={Link} to='/registro'>Registro</Nav.Link>
+                        </div>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+
+                <div className='d-none d-lg-flex'>
                     <Nav.Link as={Link} to='/login'>Login</Nav.Link>
                     <Nav.Link as={Link} to='/registro'>Registro</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+                </div>
+
+            </Nav>
         </Navbar>
     );
 }
 
-export default NavBar;
+export default NavBar
