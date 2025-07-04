08bb46d88e832420ba199d496388c32e1282150d
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-07-03 23:08:19 -0400

    feat(ProductBrowser): Agregar seccion de Productos
    Agregar seccion de busqueda de productos, compuesta por un filtro y
    visualizacion de productos.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import ProductBrowser from '../components/ProductBrowser'
 
 function HomePage() {
     return (
-        <h1>Home</h1>
+        <div className='d-flex flex-column flex-grow-1 min-h-0'>
+            <h1>Home</h1>
+            <ProductBrowser/>
+        </div>
     )
 }
 
