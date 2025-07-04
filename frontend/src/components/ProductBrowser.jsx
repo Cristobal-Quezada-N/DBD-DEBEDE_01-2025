325a6bddaebad302efaf7d086b733aceecdc76c4
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-07-03 23:11:57 -0400

    feat(Filter): Agregar la seccion de Filtro
    Agregar la seccion de filtro de la visualizacion de productos.

--- frontend/src/components/ProductBrowser.jsx
+++ frontend/src/components/ProductBrowser.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Form, Row, Col } from 'react-bootstrap'
 
+import Filter from './Filter'
+
 function ProductBrowser() {
     return (
-        <div className='d-flex w-100 h-100 min-h-0'>
-            <div className='bg-primary p-2 w-25 h-100'>
-                <h3>Filtro</h3>
+        <div className='d-flex flex-grow-1 min-h-0'>
+            <div className='bg-primary p-2 w-25'>
+                <Filter/>
             </div>
             <div className='bg-danger p-2 w-75'>
                 <h3>Productos</h3>
