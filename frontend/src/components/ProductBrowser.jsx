78fe87c097b10cfb3a33d7a8cc9469419b51972d
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-07-04 23:36:58 -0400

    feat(ProducView): Agregar vista de Productos
    Agregar vista de Productos `ProductsView` en el `ProductBrowser`.

--- frontend/src/components/ProductBrowser.jsx
+++ frontend/src/components/ProductBrowser.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Form, Row, Col } from 'react-bootstrap'
 
 import Filter from './Filter'
+import ProductsView from './ProductsView'
 
 function ProductBrowser() {
     return (
@@ -9,8 +10,12 @@ function ProductBrowser() {
             <div className='bg-primary p-2 w-25'>
                 <Filter/>
             </div>
-            <div className='bg-danger p-2 w-75'>
-                <h3>Productos</h3>
+            <div className='bg-light p-2 w-75 d-flex flex-column flex-grow-1 min-h-0'>
+                <h3 className='text-dark'>Productos</h3>
+                <br/>
+                <div className='flex-grow-1 min-h-0 overflow-auto'>
+                    <ProductsView/>
+                </div>
             </div>
         </div>
     )
