cd6979157d70e2609517761defd129cb431253ff
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-07-06 01:01:08 -0400

    chore: Test de API
    Implementar test basico de api Spring Boot a React.

--- backend/src/main/java/com/debede/backend/controller/HomeController.java
+++ backend/src/main/java/com/debede/backend/controller/HomeController.java
@@ -2,11 +2,13 @@ package com.debede.backend.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/test")
     public String home() {
-        return "Home";
+        return "Test from Spring Boot";
     }
 }
