0ebb2561cef5c76ee9092d69474f54a13b77a002
NicolasSDuran <nicolas.sarmiento.d@usach.cl>
2025-07-07 21:27:33 -0400

    Creacion de tablas
    Boleta,Tienda,etc

---
+++ backend/src/main/java/com/debede/backend/entity/Boleta.java
@@ -0,0 +1,67 @@
+package com.debede.backend.entity;
+
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "boleta")
+
+public class Boleta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_boleta;
+    private String direccion_boleta;
+    private Integer nombre_boleta;
+    private String numero_boleta;
+
+    public Boleta(Long id_boleta, String direccion_boleta, Integer nombre_boleta, String numero_boleta) {
+        this.id_boleta = id_boleta;
+        this.direccion_boleta = direccion_boleta;
+        this.nombre_boleta = nombre_boleta;
+        this.numero_boleta = numero_boleta;
+    }
+
+    public Long getId_boleta() {
+        return id_boleta;
+    }
+
+    public void setId_boleta(Long id_boleta) {
+        this.id_boleta = id_boleta;
+    }
+
+    public String getDireccion_boleta() {
+        return direccion_boleta;
+    }
+
+    public void setDireccion_boleta(String direccion_boleta) {
+        this.direccion_boleta = direccion_boleta;
+    }
+
+    public Integer getNombre_boleta() {
+        return nombre_boleta;
+    }
+
+    public void setNombre_boleta(Integer nombre_boleta) {
+        this.nombre_boleta = nombre_boleta;
+    }
+
+    public String getNumero_boleta() {
+        return numero_boleta;
+    }
+
+    public void setNumero_boleta(String numero_boleta) {
+        this.numero_boleta = numero_boleta;
+    }
+
+    @Override
+    public String toString() {
+        return "Boleta{" +
+                "nombre_boleta=" + nombre_boleta +
+                ", id_boleta=" + id_boleta +
+                ", direccion_boleta='" + direccion_boleta + '\'' +
+                ", numero_boleta='" + numero_boleta + '\'' +
+                '}';
+    }
+}
---
+++ backend/src/main/java/com/debede/backend/entity/Boleta_Producto.java
@@ -0,0 +1,44 @@
+package com.debede.backend.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "boleta_producto")
+
+public class Boleta_Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_boleta;
+    private Long id_producto;
+
+    public Boleta_Producto(Long id_boleta, Long id_producto) {
+        this.id_boleta = id_boleta;
+        this.id_producto = id_producto;
+    }
+
+    public Long getId_boleta() {
+        return id_boleta;
+    }
+
+    public void setId_boleta(Long id_boleta) {
+        this.id_boleta = id_boleta;
+    }
+
+    public Long getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(Long id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    @Override
+    public String toString() {
+        return "Boleta_Producto{" +
+                "id_boleta=" + id_boleta +
+                ", id_producto=" + id_producto +
+                '}';
+    }
+}
