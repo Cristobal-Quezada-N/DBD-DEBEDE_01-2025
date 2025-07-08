efea3654094acdc9443247ef7af8f706560b8bc1
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-07-08 14:15:27 -0400

    feat(Entity-Rol): Aplicar JPA a Rol
    Aplicar JPA a la Entidad `Rol` para habilitar funcionalidades.

--- backend/src/main/java/com/debede/backend/entity/Rol.java
+++ backend/src/main/java/com/debede/backend/entity/Rol.java
@@ -1,16 +1,30 @@
 package com.debede.backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "rol")
 public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_rol;
     private String tipo_rol;
     private Short nivel_permiso;
 
+    // Constructor vacio para JPA
+    public Rol() {}
+    // Constructor para test, debug
     public Rol(Long id_rol, String tipo_rol, Short nivel_permiso) {
         this.id_rol = id_rol;
         this.tipo_rol = tipo_rol;
         this.nivel_permiso = nivel_permiso;
     }
 
+    // Getters y Setters
     public Long getId_rol() {
         return id_rol;
     }
@@ -35,6 +49,7 @@ public class Rol {
         this.nivel_permiso = nivel_permiso;
     }
 
+    // Funcion util para debug
     @Override
     public String toString() {
         return "Rol{" +
