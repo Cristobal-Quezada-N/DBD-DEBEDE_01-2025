b5669d303f8fd5fe3f900679b44c1924c371ea72
Zhi <zhi.lu@usach.cl>
2025-07-08 21:34:54 -0400

    Arreglos

--- backend/src/main/java/com/debede/backend/entity/Valoracion_Producto.java
+++ backend/src/main/java/com/debede/backend/entity/Valoracion_Producto.java
@@ -2,70 +2,30 @@ package com.debede.backend.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "valoracion_producto")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Valoracion_Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_valoracion;
     private Long id_producto;
     private String comentario_valoracion;
     private LocalDate fecha_valoracion;
     private int puntuacion;
-
-    public Valoracion_Producto(Long id_valoracion, Long id_producto, String comentario_valoracion, LocalDate fecha_valoracion, int puntuacion) {
-        this.id_valoracion = id_valoracion;
-        this.id_producto = id_producto;
-        this.comentario_valoracion = comentario_valoracion;
-        this.fecha_valoracion = fecha_valoracion;
-        this.puntuacion = puntuacion;
-    }
-
-    public Long getId_valoracion() {
-        return id_valoracion;
-    }
-
-    public void setId_valoracion(Long id_valoracion) {
-        this.id_valoracion = id_valoracion;
-    }
-
-    public Long getId_producto() {
-        return id_producto;
-    }
-
-    public void setId_producto(Long id_producto) {
-        this.id_producto = id_producto;
-    }
-
-    public String getComentario_valoracion() {
-        return comentario_valoracion;
-    }
-
-    public void setComentario_valoracion(String comentario_valoracion) {
-        this.comentario_valoracion = comentario_valoracion;
-    }
-
-    public LocalDate getFecha_valoracion() {
-        return fecha_valoracion;
-    }
-
-    public void setFecha_valoracion(LocalDate fecha_valoracion) {
-        this.fecha_valoracion = fecha_valoracion;
-    }
-
-    public int getPuntuacion() {
-        return puntuacion;
-    }
-
-    public void setPuntuacion(int puntuacion) {
-        this.puntuacion = puntuacion;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Valoracion_Producto{" +
-                "id_valoracion=" + id_valoracion +
-                ", id_producto=" + id_producto +
-                ", comentario_valoracion='" + comentario_valoracion + '\'' +
-                ", fecha_valoracion=" + fecha_valoracion +
-                ", puntuacion=" + puntuacion +
-                '}';
-    }
 }
