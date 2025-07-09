b7f2cb98245fda19c3585d2a08b46959fab00ff0
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-07-09 18:03:02 -0400

    fix(Entity-Compra): Implementar Llaves Foraneas
    Implementar LLaves Foraneas y corregir nombre de atributos segun
    atributos de tabla correspondiente.

--- backend/src/main/java/com/debede/backend/entity/Compra.java
+++ backend/src/main/java/com/debede/backend/entity/Compra.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,10 +26,17 @@ public class Compra {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_compra;
-    private Long id_carrito;
-    private Long id_boleta;
-    private Long id_medio;
-    private Integer cantidad_comprado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_carrito")
+    private Carrito carrito;
+    @ManyToOne
+    @JoinColumn(name = "id_boleta")
+    private Boleta boleta;
+    @ManyToOne
+    @JoinColumn(name = "id_medio")
+    private Medio_De_Pago medio;
+    private Integer cantidad_comprada;
     private LocalDate fecha_compra;
     private LocalDate fecha_emision_boleta;
 }
