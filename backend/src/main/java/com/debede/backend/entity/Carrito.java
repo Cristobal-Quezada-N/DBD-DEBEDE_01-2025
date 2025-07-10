4297abfdd178bfdcf66eb299ab7367b6c2bb2861
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-07-09 21:46:34 -0400

    feat(Entity-Carrito): Implementar Lombok

--- backend/src/main/java/com/debede/backend/entity/Carrito.java
+++ backend/src/main/java/com/debede/backend/entity/Carrito.java
@@ -23,37 +23,4 @@ public class Carrito {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_carrito;
     private Integer productos_carro;
-
-    public Carrito(Long id_carrito, Integer productos_carro) {
-        this.id_carrito = id_carrito;
-        this.productos_carro = productos_carro;
-    }
-
-    public Carrito() {
-
-    }
-
-    public Long getId_carrito() {
-        return id_carrito;
-    }
-
-    public void setId_carrito(Long id_carrito) {
-        this.id_carrito = id_carrito;
-    }
-
-    public int getProductos_carro() {
-        return productos_carro;
-    }
-
-    public void setProductos_carro(Integer productos_carro) {
-        this.productos_carro = productos_carro;
-    }
-
-    @Override
-    public String toString() {
-        return "Carrito{" +
-                "id_carrito=" + id_carrito +
-                ", productos_carro=" + productos_carro +
-                '}';
-    }
 }
